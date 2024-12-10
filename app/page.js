0617add415de1a09536f8b589fbea269d892b9f6
carldouch <goldstraitjacket@gmail.com>
2024-12-10 17:47:55 +0100

    hide fps stat

--- app/page.js
+++ app/page.js
@@ -66,7 +66,7 @@ export default function Home() {
             )}
           </Suspense>
         </ScrollControls>
-        <Stats />
+        {/* <Stats /> */}
       </Canvas>
       {/* <Timeline
         onSectionChange={handleSectionChange}
